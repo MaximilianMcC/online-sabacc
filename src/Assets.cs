6cdf07c1726c659c332a73dca6ff998e54f1147c
Kai <71107159+MilkyBruv@users.noreply.github.com>
2024-05-07 22:06:54 +1200

    started on card texture crap

--- src/Assets.cs
+++ src/Assets.cs
@@ -4,13 +4,16 @@ class Assets
 {
 	public static Font FontEnglish;
 	public static Font FontAurebesh;
-
+	public static Texture2D CardGeneratorSpriteSheet;
 
 	public static void LoadAssets()
 	{
 		// Load both the english, and aurebesh fonts
 		FontEnglish = Raylib.LoadFont("./assets/fonts/english.ttf");
 		FontAurebesh = Raylib.LoadFont("./assets/fonts/aurebesh.ttf");
+
+		// Load textures
+		CardGeneratorSpriteSheet = Raylib.LoadTexture("./assets/textures/cards_spritesheet.png");
 	}
 
 	public static void UnloadAssets()
